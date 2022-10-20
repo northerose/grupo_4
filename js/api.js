2e84c6bcac99c0566f1d5475f812d4e33deca898
northerose <roselinjimenez1@gmail.com>
2022-10-20 19:59:13 -0300

    responsive website completo parte I

---
+++ js/api.js
@@ -0,0 +1,18 @@
+function getDatos () {
+    var resultado = [];
+    // Simple response handling
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function(response) {
+        return response.json();
+    }).then(function(resultado) {
+        // Yay, `j` is a JavaScript object
+        // console.log(j); 
+        let elem = document.querySelector ( '.tabla_rest' )
+        for (res in resultado) {
+            titulo = resultado[res].title
+            elem.innerHTML = elem.innerHTML +  '<tr>'+titulo+'</tr><br>';
+        }
+    });
+}
+
+getDatos()
